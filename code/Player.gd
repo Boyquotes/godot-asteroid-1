a88da2939a7d5e32c64db973c1a515f3e5a56049
Greg Freitas <gregfreitas1997@gmail.com>
2023-03-30 12:19:58 -0400

    Updated player to use top down movement

--- code/Player.gd
+++ code/Player.gd
@@ -3,26 +3,37 @@ extends Area2D
 signal hit
 
 @export var speed = 400
+var screen_size: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	screen_size = get_viewport_rect().size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var velocity = move()
+	
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+	position += velocity * delta
+
+func move() -> Vector2:
 	var velocity = Vector2.ZERO
 	
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-	
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-	
-	position += velocity * delta
 
+	return velocity
 
 func respawn(pos):
 	position = pos
